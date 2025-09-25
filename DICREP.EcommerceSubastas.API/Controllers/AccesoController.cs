acbd8f94d8e869a05c7a7ee35c8537ca863a7bef
samiraaaw <sa.awad@duocuc.cl>
2025-09-25 05:48:46 -0300

    holaa

---
+++ DICREP.EcommerceSubastas.API/Controllers/AccesoController.cs
@@ -0,0 +1,23 @@
+﻿using DICREP.EcommerceSubastas.API.Attributes;
+using DICREP.EcommerceSubastas.Application.DTOs.Auth;
+using DICREP.EcommerceSubastas.Application.UseCases.Acceso;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DICREP.EcommerceSubastas.API.Controllers
+{
+    [DevelopmentOnly]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccesoController : ControllerBase
+    {
+        private readonly GetAllFuncionalidadesUseCase _getAllFuncionalidadesUseCase;
+
+        public AccesoController(GetAllFuncionalidadesUseCase getAllFuncionalidadesUseCase)
+        {
+            _getAllFuncionalidadesUseCase = getAllFuncionalidadesUseCase; 
+        }
+
+    }
+}
